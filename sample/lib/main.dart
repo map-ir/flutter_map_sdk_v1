8db13796f175c7ca93b0a02007f32f5db0152283
Morteza Hoseini <mortezahosseinee@gmail.com>
2020-09-27 14:59:57 +0330

    remove key

--- sample/lib/main.dart
+++ sample/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_map_sdk_v1/flutter_map_sdk_v1.dart';
 
 void main() => runApp(MyApp());
@@ -12,8 +11,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    MapirMap.init("mapir_sdk_sample",
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6Ijk5MzM4ZTg4ZDQ4OWRkYWU0MDViMjEwNWExMzA1YjZlYWFmZjQ5ZTkxZjk4NjYxMTk4ZDhiMDZiOTc2Mjc5MmRlOWFmMjQwN2IzNzVjNzNmIn0.eyJhdWQiOiI5NDQ5IiwianRpIjoiOTkzMzhlODhkNDg5ZGRhZTQwNWIyMTA1YTEzMDViNmVhYWZmNDllOTFmOTg2NjExOThkOGIwNmI5NzYyNzkyZGU5YWYyNDA3YjM3NWM3M2YiLCJpYXQiOjE1OTkzNzkwMDgsIm5iZiI6MTU5OTM3OTAwOCwiZXhwIjoxNjAxOTc0NjA4LCJzdWIiOiIiLCJzY29wZXMiOlsiYmFzaWMiXX0.ovF71cQXJ0PFu1rh_cLUCiXUEF5XpuxSMMI8DMcEJLJ8G-pMC9owTqX_7HmvB4dN_ZeF7VXzGouFzD2gfMgd6mvM6jHCH-T8HFH8aTER-rAXg2H8ZLqBaPNslpDLDZ-mHdBRvVCSThNtqlILeO8QJhCxP4-oO3Qcq4LTZ73x5OebZdGzcUGEiFHaJsSP2VYb2SAggkeQzMdmkxWFxXbtfu7X6tOfj-JFuNR3Dcdq6p95FIaE_WZBJ40kIxIvNaEumZG9z1jIc-t0Ba-huUFqBDn1Svrvp6RiSLMyshHYTtGlVDmyRKE_ojapGS2v-EJ6T7jQxeUSbNYAQhDdyhrVHQ");
+    MapirMap.init("نام پکیج برنامه شما", "کلید دسترسی");
 
     var mapController = MapController();
     var statefulMapController =
@@ -27,73 +25,36 @@ class _MyAppState extends State<MyApp> {
       anchorPos: AnchorPos.align(AnchorAlign.center),
     );
 
-    // statefulMapController.addStatefulMarker(
-    //     name: "sampleMarker1",
-    //     statefulMarker: StatefulMarker(
-    //         height: 80.0,
-    //         width: 120.0,
-    //         state: <String, dynamic>{"showText": false},
-    //         point: LatLng(35.713949, 51.399086),
-    //         builder: (BuildContext context, Map<String, dynamic> state) {
-    //           Widget w;
-    //           final markerIcon = IconButton(
-    //               icon: const Icon(Icons.location_on),
-    //               onPressed: () => statefulMapController.mutateMarker(
-    //                   name: "sampleMarker1",
-    //                   property: "showText",
-    //                   value: !(state["showText"] as bool)));
-    //           if (state["showText"] == true) {
-    //             w = Column(children: <Widget>[
-    //               markerIcon,
-    //               Container(
-    //                   color: Colors.transparent,
-    //                   child: Text("sampleMarker1", textScaleFactor: 1))
-    //             ]);
-    //           } else
-    //             w = markerIcon;
-    //
-    //           return w;
-    //         }));
-    //
-    // var points = List<LatLng>();
-    // points.add(LatLng(35.713949, 51.399086));
-    // points.add(LatLng(35.714345, 51.400304));
-    // points.add(LatLng(35.712942, 51.400486));
-    //
-    // statefulMapController.addLine(
-    //     name: "sampleLine1",
-    //     points: points,
-    //     width: 5.0,
-    //     color: Colors.blue,
-    //     isDotted: true);
-    //
-    // statefulMapController.addPolygon(
-    //     name: "samplePolygon1",
-    //     points: points,
-    //     color: Colors.blue,
-    //     borderWidth: 5.0,
-    //     borderColor: Colors.red);
+    var points = List<LatLng>();
+    points.add(LatLng(35.713949, 51.399086));
+    points.add(LatLng(35.714345, 51.400304));
+    points.add(LatLng(35.712942, 51.400486));
 
-    statefulMapController.addMarker(marker: marker1, name: "s1");
+    statefulMapController.addLine(
+        name: "sampleLine1",
+        points: points,
+        width: 5.0,
+        color: Colors.blue,
+        isDotted: true);
+
+    statefulMapController.addPolygon(
+        name: "samplePolygon1",
+        points: points,
+        color: Colors.blue,
+        borderWidth: 5.0,
+        borderColor: Colors.red);
+
+    statefulMapController.addMarker(marker: marker1, name: "sampleMarker1");
 
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: MapirMapView(
             mapController: mapController,
             statefulMapController: statefulMapController,
-            onMapReady: () {
-              loadData(statefulMapController);
-            },
+            onMapReady: () {},
             onMapClick: (location) {},
             onMapLongClick: (location) {},
             onLocationUpdate: (location) {},
-            showLocationMarker: true));
-  }
-
-  void loadData(statefulMapController) async {
-    print("Loading geojson data");
-    final data = await rootBundle.loadString('assets/test.geojson');
-    await statefulMapController.fromGeoJson(data,
-        markerIcon: Icon(Icons.local_airport), verbose: true);
+            showLocationMarker: false));
   }
 }
